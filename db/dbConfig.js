7cfb5a3fbef5d4b3b2cbb2be86bd11eab3e3fdad
Fetu Deen <fettabin@gmail.com>
2024-10-21 02:39:11 +0300

    new

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -9,11 +9,7 @@
 //   connectionLimit: 10,
 // });
 
-
-
-
 // module.exports = dbConnection.promise();
-
 
 const mysql2 = require("mysql2");
 const dotenv = require("dotenv");
@@ -25,6 +21,7 @@ const dbConnection = mysql2.createPool({
   host: process.env.HOST, // Ensure this uses the HOST variable from .env
   password: process.env.PASSWORD,
   connectionLimit: 10,
+  port: process.env.PORT,
 });
 
 module.exports = dbConnection.promise();
