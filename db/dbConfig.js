933c409bc56cf97cbe4abf74df934e5ea41fc65a
Fetu Deen <fettabin@gmail.com>
2024-10-21 02:33:56 +0300

    initial on my new repo

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,15 +1,30 @@
+// const mysql2 = require("mysql2");
+// const dotenv = require("dotenv");
+// dotenv.config()
+// const dbConnection = mysql2.createPool({
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   host: "localhost",
+//   password: process.env.PASSWORD,
+//   connectionLimit: 10,
+// });
+
+
+
+
+// module.exports = dbConnection.promise();
+
+
 const mysql2 = require("mysql2");
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
+
 const dbConnection = mysql2.createPool({
   user: process.env.USER,
   database: process.env.DATABASE,
-  host: "localhost",
+  host: process.env.HOST, // Ensure this uses the HOST variable from .env
   password: process.env.PASSWORD,
   connectionLimit: 10,
 });
-
-
-
 
 module.exports = dbConnection.promise();
